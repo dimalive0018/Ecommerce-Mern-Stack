e6551fc15350e11b75adb8cc46de089d058eb02e
liviodimola <109294880+liviodimola@users.noreply.github.com>
2023-08-12 20:51:04 +0200

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const stripeRoute = require('./Routes/stripe.route');
 const cors = require('cors');
 const multer = require('multer');
 require('colors');
+const path = require('path');
 
 dotenv.config();
 
@@ -28,17 +29,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/category', categoryRoute);
 app.use('/api/v1/product', productRoute);
 app.use('/api/v1/stripe', stripeRoute);
 
-app.get('/', (req, res, next) => {
-    res.send({
-        message: 'Benvenuto in questo ecommerce'
-    });
-});
+app.use('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+})
 
 app.use((req, res, next) => {
     next(httpErrors(404, 'Non trovato'));
